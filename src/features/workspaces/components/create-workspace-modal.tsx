ad29046e75aa491a80130971d5402bbca45fc2ea
andriel300 <andrieldata@proton.me>
2024-09-03 23:25:26 -0300

    feat(workspaces): integrate workspace creation with modal and redirection
    - Added form handling to `CreateWorkspaceModal` with
    `useCreateWorkspace` for creating a new workspace.
    - Implemented submission logic to call the `mutate` function, triggering
    workspace creation and redirecting to the newly created workspace upon
    success.
    - Managed form input state for workspace name and disabled form elements
    during pending states.

--- src/features/workspaces/components/create-workspace-modal.tsx
+++ src/features/workspaces/components/create-workspace-modal.tsx
@@ -3,38 +3,59 @@
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
 import { useCreateWorkspaceModal } from "../store/use-create-workspace-modal";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { useCreateWorkspace } from "../api/use-create-workspace";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
 
 export const CreateWorkspaceModal = () => {
+  const router = useRouter();
   const [open, setOpen] = useCreateWorkspaceModal();
+  const [name, setName] = useState("");
+
+  const { mutate, isPending } = useCreateWorkspace();
 
   const handleClose = () => {
     setOpen(false);
-    // TODO: Clear form
+    setName("");
   };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    mutate({ name }, {
+      onSuccess: (id) => {
+        router.push(`/workspace/${id}`);
+        handleClose();
+      }
+    });
+  };
+
+
   return (
     <Dialog open={open} onOpenChange={handleClose}>
       <DialogContent>
         <DialogHeader>
           <DialogTitle>Add a Workspace</DialogTitle>
         </DialogHeader>
-        <form className="space-y-4">
+        <form onSubmit={handleSubmit} className="space-y-4">
           <Input
-            value=""
-            disabled={false}
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            disabled={isPending}
             required
             autoFocus
             minLength={3}
             placeholder="Workspace name e.g. 'Work', 'Personal', 'Home'"
           />
           <div className="flex justify-end">
-            <Button disabled={false} className="">
+            <Button disabled={isPending}>
               Create
             </Button>
           </div>
