1fb377e5ab14b0ea8df8dcf132ff836238be5406
andriel300 <andrieldata@proton.me>
2024-09-03 23:24:06 -0300

    feat(workspaces): implement workspace redirection and workspace page
    - Added automatic redirection to the workspace page when a workspace is
    detected on the home page.
    - Used Next.js `router.replace` to navigate users to
    `/workspace/{workspaceId}` after workspace detection.
    - Created `WorkspaceIdPage` component to handle dynamic routing for
    workspaces, displaying the workspace ID as a placeholder.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { useEffect, useMemo } from "react";
 import UserButton from "@/features/auth/components/user-button";
 import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
 import { useCreateWorkspaceModal } from "@/features/workspaces/store/use-create-workspace-modal";
 
 export default function Home() {
+  const router = useRouter();
   const [open, setOpen] = useCreateWorkspaceModal();
   const { data, isLoading } = useGetWorkspaces();
 
@@ -15,11 +17,12 @@ export default function Home() {
     if (isLoading) return;
 
     if (workspaceId) {
-      console.log("Redirect to workspace");
+      router.replace(`/workspace/${workspaceId}`);
     } else if (!open) {
       setOpen(true);
     }
-  }, [workspaceId, isLoading, open, setOpen]);
+  }, [workspaceId, isLoading, open, setOpen, router]);
+
   return (
     <div>
       <UserButton />
