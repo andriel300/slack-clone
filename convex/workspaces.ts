4040df1ffb68d0c3465540e6ab5e9af84007286f
andriel300 <andrieldata@proton.me>
2024-09-03 23:23:22 -0300

    feat(workspaces): add workspace creation mutation with authentication
    - Added `create` mutation to handle workspace creation with user
    authentication.
    - Generated a temporary `joinCode` during workspace creation (to be
    improved later).
    - Integrated authentication check using `getAuthUserId` to ensure only
    authenticated users can create workspaces.
    - Workspace creation inserts a new record into the `workspaces` table
    with the provided name and associated user.

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -1,4 +1,31 @@
-import { query } from "./_generated/server";
+import { query, mutation } from "./_generated/server";
+import { getAuthUserId } from "@convex-dev/auth/server";
+
+import { v } from "convex/values";
+
+export const create = mutation({
+  args: {
+    name: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+
+    if (!userId) {
+      throw new Error("Anauthorized");
+    }
+
+    // TODO: Create a proper method later
+    const joinCode = "123456";
+
+    const workspaceId = await ctx.db.insert("workspaces", {
+      name: args.name,
+      userId,
+      joinCode,
+    });
+
+    return workspaceId;
+  },
+})
 
 export const get = query({
   args: {},
