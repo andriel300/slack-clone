0c38efd100c943094b39e897c8e6c7f87c78ad03
andriel300 <andrieldata@proton.me>
2024-09-28 19:24:02 -0300

    feat(workspaces): add member assignment and workspace filtering by user
    - Implemented a `generateCode` function to create random join codes for
    new workspaces.
    - When a workspace is created, the creating user is automatically
    assigned as an `admin` in the `members` table.
    - Updated the `get` function to return only the workspaces where the
    authenticated user is a member, using the `members` table.
    - Added a check in the `getById` function to ensure the user is a member
    of the requested workspace before returning it. If the user is not a
    member, `null` is returned.

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -3,6 +3,16 @@ import { getAuthUserId } from "@convex-dev/auth/server";
 
 import { v } from "convex/values";
 
+const generateCode = () => {
+  const code = Array.from(
+    { length: 6 },
+    () =>
+      "0123456789abcdefghijklmnopqrstuvwxyz"[Math.floor(Math.random() * 36)]
+  ).join("");
+
+  return code;
+}
+
 export const create = mutation({
   args: {
     name: v.string(),
@@ -14,13 +24,18 @@ export const create = mutation({
       throw new Error("Anauthorized");
     }
 
-    // TODO: Create a proper method later
-    const joinCode = "123456";
+    const joinCode = generateCode();
 
     const workspaceId = await ctx.db.insert("workspaces", {
       name: args.name,
       userId,
       joinCode,
+    });
+
+    await ctx.db.insert("members", {
+      userId,
+      workspaceId,
+      role: "admin",
     });
 
     return workspaceId;
@@ -30,7 +45,29 @@ export const create = mutation({
 export const get = query({
   args: {},
   handler: async (ctx) => {
-    return await ctx.db.query("workspaces").collect();
+    const userId = await getAuthUserId(ctx);
+    if (!userId) {
+      return [];
+    }
+
+    const member = await ctx.db
+      .query("members")
+      .withIndex("by_user_id", (q) => q.eq("userId", userId))
+      .collect();
+
+    const workspaceIds = member.map((member) => member.workspaceId);
+
+    const workspaces = [];
+
+    for (const workspaceId of workspaceIds) {
+      const workspace = await ctx.db.get(workspaceId);
+
+      if (workspace) {
+        workspaces.push(workspace);
+      }
+    }
+
+    return workspaces;
   },
 });
 
@@ -43,6 +80,17 @@ export const getById = query({
 
     if (!userId) {
       throw new Error("Unauthorized");
+    }
+
+    const member = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", args.id).eq("userId", userId),
+      )
+      .unique();
+
+    if (!member) {
+      return null;
     }
 
     return await ctx.db.get(args.id);
